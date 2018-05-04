9a049a2e3dac4d79c4f413e952d96ddd079f90b5
Johnn3volta <johnn3volta@gmail.com>
2018-05-04 17:10:07 +0300

    Новые файлы

---
+++ yii2/common/behaviors/Slug.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ContentManager5
+ * Date: 04.05.2018
+ * Time: 11:41
+ */
+
+class Slug{
+
+}
---
+++ yii2/frontend/controllers/ProgramsCatsController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ContentManager5
+ * Date: 04.05.2018
+ * Time: 15:57
+ */
+
+namespace frontend\controllers;
+
+
+class ProgramsCatsController{
+
+}
