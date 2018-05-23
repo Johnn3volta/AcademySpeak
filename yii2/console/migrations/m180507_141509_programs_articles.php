8a2207495c370278cac20ab8d8a85a0810abaf5a
johnn3volta <johnn3volta@gmail.com>
2018-05-23 23:11:53 +0300

    миграция

--- yii2/console/migrations/m180507_141509_programs_articles.php
+++ yii2/console/migrations/m180507_141509_programs_articles.php
@@ -16,24 +16,26 @@ class m180507_141509_programs_articles extends Migration{
             'name'        => $this->string()->notNull(),
             'seo_h1'      => $this->string()->notNull(),
             'text'        => $this->text()->notNull(),
+            'text2'       => $this->text(),
             'title'       => $this->string()->notNull(),
             'description' => $this->string()->notNull(),
             'keywords'    => $this->string()->notNull(),
             'url'         => $this->string()->notNull(),
             'parent_id'   => $this->integer(),
+            'price'       => $this->integer(),
             'created_at'  => $this->integer(),
             'updated_at'  => $this->integer(),
         ]);
 
         $this->createIndex('idx-programs_acrticles-parent_id', 'programs_articles', 'parent_id');
-        $this->addForeignKey('fk-programs_articles-parent_id', 'programs_articles', 'parent_id', 'programs_cats', 'id','SET NULL');
+        $this->addForeignKey('fk-programs_articles-parent_id', 'programs_articles', 'parent_id', 'programs_cats', 'id', 'SET NULL');
     }
 
     /**
      * {@inheritdoc}
      */
     public function safeDown(){
-        $this->dropForeignKey('fk-programs_articles-parent_id','programs_articles');
+        $this->dropForeignKey('fk-programs_articles-parent_id', 'programs_articles');
         $this->dropIndex('idx-programs_acrticles-parent_id', 'programs_articles');
         $this->dropTable('programs_articles');
 
