c34c018679eac8e02d2db502e2236e5cdeeec3fb
Johhn3volta <johnn3volta@gmail.com>
2018-05-25 17:55:00 +0300

    Добавил тэг роботс

--- yii2/frontend/controllers/MetaController.php
+++ yii2/frontend/controllers/MetaController.php
@@ -9,9 +9,10 @@ use yii\web\Controller;
 
 class MetaController extends Controller{
 
-    protected function setMeta($title = null,$description = null,$keywords = null, $type = null){
+    protected function setMeta($title = null,$description = null,$keywords = null, $type = null,$robots = 'index,follow'){
         $this->getView()->title = ($title != null) ? $title.' | '.Yii::$app->request->serverName : $title;
         $this->getView()->registerLinkTag(['rel' => 'canonical','href' => 'http://'.Yii::$app->request->serverName.Yii::$app->request->url]);
+        $this->getView()->registerMetaTag(['name' => 'robots','content' => $robots],'robots');
         $this->getView()->registerMetaTag(['name' => 'description','content' => $description],'description');
         $this->getView()->registerMetaTag(['name' => 'keywords','content' => $keywords ],'keywords');
         $this->getView()->registerMetaTag(['property' => 'og:url','content' => 'http://'.Yii::$app->request->serverName.Yii::$app->request->url]);
